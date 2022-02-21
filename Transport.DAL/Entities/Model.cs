60dfa51e2445679e0a4c254887165b37cec6558e
Artur Hostiuk <artur1100011@gmail.com>
2022-02-21 09:09:20 +0200

    added Make, Model Entities

---
+++ Transport.DAL/Entities/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transport.DAL.Interfaces.EntitiyInterface;
+
+namespace Transport.DAL.Entities
+{
+    public class Model : IEntity<int>
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int MakeId { get; set; }
+    }
+}
