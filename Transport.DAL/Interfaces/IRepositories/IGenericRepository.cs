857e54ee2aa84def3e2815831682fe50e1396f53
Artur Hostiuk <artur1100011@gmail.com>
2022-02-21 10:58:51 +0200

    added Ado Dapper Generic Repository

---
+++ Transport.DAL/Interfaces/IRepositories/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transport.DAL.Interfaces.EntitiyInterface;
+
+namespace Transport.DAL.Interfaces.IRepositories
+{
+    public interface IGenericRepository<TEntity, TId> where TEntity : IEntity<TId>
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+
+        Task<TEntity> Get(TId Id);
+
+        Task<int> Add(TEntity entity);
+
+        Task Update(TEntity entity);
+
+        Task Delete(TEntity entity);
+    }
+}
