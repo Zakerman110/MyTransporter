92aa9d7762dadb1710dc205a59193de1b88c38ed
Artur Hostiuk <artur1100011@gmail.com>
2022-03-28 11:08:08 +0300

    added generic repository and EFCore packages

---
+++ Order.DAL/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using Order.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.DAL.Interfaces.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task Create(TEntity item);
+        Task<TEntity> FindById(int id);
+        Task<IEnumerable<TEntity>> Get();
+        Task<IEnumerable<TEntity>> Get(Func<TEntity, bool> predicate);
+        Task Remove(TEntity item);
+        Task Update(TEntity item);
+    }
+}
---
+++ Order.DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using Order.DAL.Entities;
+using Order.DAL.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.DAL.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        public Task Create(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> Get(Func<TEntity, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remove(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
