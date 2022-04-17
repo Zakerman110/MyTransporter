181e41595f423272dcaf9c508451fb5b15fae630
Artur Hostiuk <artur1100011@gmail.com>
2022-04-17 23:10:57 +0300

    added automapper manual mapping

--- Order.BLL/DTO/Responses/OrderResponse.cs
+++ Order.BLL/DTO/Responses/OrderResponse.cs
@@ -10,8 +10,8 @@ namespace Order.BLL.DTO.Responses
     public class OrderResponse
     {
         public int Id { get; set; }
-        public DateTime OrderDate { get; set; }
-        public OrderStatus OrderStatus { get; set; }
+        public DateTime PlaceDate { get; set; }
+        public OrderStatus Status { get; set; }
         public int CustomerId { get; set; }
         public int VehicleId { get; set; }
         public RouteResponse Route { get; set; }
